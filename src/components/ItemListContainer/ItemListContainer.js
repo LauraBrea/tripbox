9fb1d0b23d9d3f48a23ab18b9f0ae3639b80b243
Laura Brea <arq.laurabrea@gmai.com>
2022-02-05 09:27:02 -0300

    desafio 5 mas useEffect

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,63 +1,45 @@
 import React,{useEffect,useState} from 'react';
-import './ItemListContainer.css';
+import { stock } from '../BaseDatosItems/stock';
 import { ItemList } from './ItemList/ItemList.js';
+import './ItemListContainer.css';
 
 
-export const ItemListContainer = ({greeting}) => {
+export const pedirDatos = () => {
+    return new Promise( (resolve, reject) => {
+        setTimeout(() => {
+            resolve(stock)
+        }, 2500)
+    })
+}
 
-    const getItems = () =>{
-        return new Promise ((resolve) =>{
+export const ItemListContainer = () => {
+    
+    const [items, setItems] = useState([])
+    const [loading, setLoading] = useState(false)
 
-            setTimeout(()=> resolve(
-            [{id:1, 
-            img: 'https://images.unsplash.com/photo-1639502153551-2ea0443fcf0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=912&q=80',
-            name: 'Staycation Anselmo', 
-            category:'Bienestar',
-            description: 'Accedé a un hotel de lujo durante todo un día y escapá de la rutina.', 
-            price:'$8600'},
-            {
-            id:2, 
-            img: 'https://images.unsplash.com/photo-1643133277936-9f93d8792522?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-            name: 'Big Pons Restó', 
-            category:'Gastronomía',
-            description: 'Cocina de alta calidad en un ambiente sumamante agradable y familiar.', 
-            price:'$3500'},
-            {
-            id:3, 
-            img: 'https://images.unsplash.com/photo-1558347718-b45a32c31c03?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
-            name: 'Angostura Resort', 
-            category:'Escapadas',
-            description: 'Una escapada ideal para desconectarte entre la montaña y el lago.', 
-            price:'$28900'},
-            {id:4, 
-            img: 'https://images.unsplash.com/photo-1631839686574-2497d4b71873?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
-            name: 'Una noche francesa', 
-            category:'Escapadas',
-            description: 'Un antiguo edificio de 1910, convertido en un hotel boutique urbano.', 
-            price:'$11900'}
-            ]
+    useEffect( () => {
+        setLoading(true)
 
-            ),2000)
-        })
-    }
+        pedirDatos()
+            .then((res) => {
+                setItems( res )
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+            .finally(() => {
+               setLoading(false)
+            })
 
-    const [items, setItems] = useState([]);
-
-    useEffect(()=>{
-        getItems().then(data =>{
-            setItems(data)})
-    },[])
-
+    }, [])
 
     return <div className="itemListContainer">
                 <section>
-                    {greeting}
-
                     <h1>Descubrí Tripbox</h1>
-                    <p>Tripbox es una forma diferente de vivir emociones y experiencias únicas. <br></br> Sumate a la experiencia!</p>
-
+                    <p>Tripbox es una forma diferente de vivir emociones y experiencias únicas. <br></br> Vivilo. Disfrutá. Regalá!</p>
                 </section>
-
-                <ItemList items={items}/>   
+                {loading ? <h2 className='loading'>Cargando...</h2> : <ItemList items={items}/> } 
             </div>
 }
+
+
